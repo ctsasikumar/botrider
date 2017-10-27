35105c5d84244d8ed8a4bed52a5492941dcb8e73
Sasikumar <sasi@social-gear.jp>
2017-10-27 11:11:38 +0800

    [All]

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'verify_token' => '3312sasi',
+    'token' => 'EAAGgZCAcTBRIBAC0Ui97iMUbqK58VOS5mkc4fessixH6NBkBZAgQtXQkcp7EexWYgrFjxd046wanULBVzFZAWZBwWNnlnwWdZCiqgMPqPplvfdEpxxZApYKVKMqxJhyVZCsXBIU3WhJ91EhHDp4rdH1RWnSZBFH15nL3TdhGetg02QZDZD'
+];
